86dd340132648fd1f5ee2545e46a375e1d39aa3b
david-jankoski <leledavid@gmail.com>
2018-04-10 16:49:01 +0200

    bit better seq tests script

--- time_seq_curl_calls.sh
+++ time_seq_curl_calls.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+#
 # Run this script from cmd line and pipe output to txt file
 # ./time_seq_curl_calls.sh > time_seq_curl_calls.txt
 # will produce a 1 column 300 row matrix of response times.
+# NOTE: Modify the following 2 args to fit to your settings.
+
+# url to be tested
+testurl=http://localhost:8004/ocpu/library/mylibrary/R/predict/json
+
+# any additional stuff like e.g. args to curl (optional - can be empty)
+additonalres="-F arg1=@some_file.json"
 
 for i in `seq 1 300`;
 do
- curl -X POST http://192.168.1.207/ocpu/library/hellotripv2/R/predict/json -F "lookup_result=@lookup_result.json" -o /dev/null -s -w %{time_total}\\n
+ curl -X POST $testurl $additonalres -o /dev/null -s -w %{time_total}\\n
 done
